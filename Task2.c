3982230784a911331c6cd16db2b4b0757aec5770
Andrey <a.kuropatov1@g.nsu.ru>
2023-03-07 15:21:29 +0000

    Task2

--- Task2.c
+++ Task2.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+//#include "nvToolsExt.h"
+//#include <nvtx3/nvToolsExt.h>
 
 int iter;
 
@@ -10,22 +12,19 @@ float searchMax(float** arrcur, float** arrprev, int size) {
 	float** arrdelta = malloc(sizeof(float*) * size);
 	float max = 0;
 
-	#pragma acc kernels
-	{
+	#pragma acc loop vector
 	for (int i = 0; i < size; i++) {
 		arrdelta[i] = malloc(sizeof(float) * size);
 	}
-	}
 	iter++;
 
-	#pragma acc parallel
-	{
+	#pragma acc loop gang
 	for (int i = 0; i < size; i++) {
+		#pragma acc loop vector
 		for (int j = 0; j < size; j++) {
 			arrdelta[i][j] = fabs(arrcur[i][j] - arrprev[i][j]);
 			if (arrdelta[i][j] > max) max = arrdelta[i][j];
 		}
-	}
 	}
 
 	return max;
@@ -36,14 +35,12 @@ void arrayCompute(int size, float** arr){
 	arr[0][0] = 10, arr[0][k] = 20;
 	arr[k][0] = 30, arr[k][k] = 20;
 
-	#pragma acc kernels
-	{
+	#pragma acc loop vector
 	for (int i = 1; i < k; i++) {
 		arr[0][i] = fabs(arr[0][0] - arr[0][k]) / k;
 		arr[k][i] = fabs(arr[k][0] - arr[k][k]) / k;
 		arr[i][0] = fabs(arr[0][0] - arr[k][0]) / k;
 		arr[i][k] = fabs(arr[0][k] - arr[k][k]) / k;
-	}
 	}
 
 	int i = 1, j = 1;
@@ -61,7 +58,9 @@ void arrayCompute(int size, float** arr){
 	iter++;
 }
 
-int main(int argc, char** argv){
+int main(int argc, char* argv[]){
+//	nvtxRangePushA("GPU Time");
+
 	clock_t begin = clock();
 
 	float exact = atof(argv[1]), err = INT_MAX;
@@ -72,13 +71,11 @@ int main(int argc, char** argv){
 
 		arrcur = malloc(sizeof(float*) * size);
 		arrprev = malloc(sizeof(float*) * size);
-
-		#pragma acc kernels
-		{
+	
+		#pragma acc loop vector
 		for(int i = 0; i < size; i++){
 			arrcur[i] = malloc(sizeof(float) * size);
 			arrprev[i] = malloc(sizeof(float) * size);
-		}
 		}
 
 		arrayCompute(size, arrcur);
@@ -88,12 +85,10 @@ int main(int argc, char** argv){
 
 		if(maxError < err) err = maxError;
 		
-		#pragma acc kernels
-		{
+		#pragma acc loop vector
 		for(int i = 0; i < size; i++){
 			free(arrcur[i]);
 			free(arrprev[i]);
-		}
 		}
 
 		free(arrcur);
@@ -107,6 +102,8 @@ int main(int argc, char** argv){
 	double time_elapsed = (double)(end - begin) / CLOCKS_PER_SEC;
 
 	printf("%lf", time_elapsed);
+
+//	nvtxRangePop();
 
 	return 0;
 }
