c8f12c5aaf3e4580b3e08dc53f4b0006522b35a5
Andrey <a.kuropatov1@g.nsu.ru>
2023-05-02 06:58:36 +0000

    Task3

--- Task3_new.c
+++ Task3_new.c
@@ -4,12 +4,22 @@
 #include <limits.h>
 #include <cublas_v2.h>
 
+void printez(float* arr, int size){
+	for(int i = 0; i < size; i++){
+		for(int j = 0; j < size; j++){
+			printf("%lf ", arr[i * size + j]);
+		}
+	}
+}
+
 int main(int argc, char** argv){
 	clock_t begin = clock();
 
+	//Handle and status
 	cublasHandle_t handler;
 	cublasStatus_t status;
 
+	//Initialize a context
 	status = cublasCreate(&handler);
 	if(status == CUBLAS_STATUS_SUCCESS){
 		printf("Success!\n");
@@ -19,6 +29,7 @@ int main(int argc, char** argv){
 		exit(0);
 	}
 
+	//initialization
 	float err = 1.0, exact = 0.000001;
 	int size = atoi(argv[1]), itermax = atoi(argv[2]), iter = 0;
 
@@ -33,6 +44,7 @@ int main(int argc, char** argv){
 		}
 	}
 
+	//initial fill of the array
 	int k = size - 1;
 	arrprev[0] = 10;
 	arrprev[k] = 20;
@@ -46,6 +58,7 @@ int main(int argc, char** argv){
 		arrprev[i * size + k] = arrprev[(i - 1) * size + k] + step;
 	}
 
+
 	//#pragma acc data copyin(arrprev[:size], arrnew[:size], arrerr[:size])
 	for(int i = 0; i < size; i++){
 		for(int j = 0; j < size; j++){
@@ -53,18 +66,27 @@ int main(int argc, char** argv){
 		}
 	}
 
-	#pragma acc data copy(err) copyin(arrprev[:size], arrnew[:size], arrerr[:size])
+	#pragma acc data copy(err) copyin(arrprev[:size*size], arrnew[:size*size], arrerr[:size*size])
 	{
 	while(iter < itermax && err > exact){
 		iter++;
 		double alpha = -1.0;
 		int index = 0;
 
+		for(int i = 0; i < size; i++){
+			for(int j = 0; j < size; j++){
+				#pragma acc kernels
+				printf("%lf ", arrnew[i * size + j]);
+			}
+			printf("\n");
+		}
+		printf("\n");
+
 		#pragma acc data present(arrprev, arrnew)
 		//#pragma acc parallel loop independent collapse(2) vector vector_length(256) gang num_gangs(256)
 		#pragma acc parallel loop gang worker num_workers(4) vector_length(128)
-		for(int i = 0; i < size; i++){
-			for(int j = 0; j < size; j++){
+		for(int i = 1; i < size - 1; i++){
+			for(int j = 1; j < size - 1; j++){
 				int n = i * size + j;
 				int w = (i - 1) * size + j;
 				int x = (i + 1) * size + j;
@@ -74,39 +96,59 @@ int main(int argc, char** argv){
 			}
 		}
 
-		//status = cublasDcopy(handler, size, arrnew, 1, arrerr, 1);
-
-		//#pragma acc data present(arrprev, arrerr)
-
+		//Calculating error every 100 iterations
 		if(iter % 100 == 0){
+			for(int i = 0; i < size; i++){
+				for(int j = 0; j < size; j++){
+					#pragma acc kernels
+					printf("%lf ", arrerr[i * size + j]);
+				}
+				printf("\n");
+			}
+			printf("\n");
+
 			#pragma acc data present(arrprev, arrnew, arrerr)
 			#pragma acc host_data use_device(arrprev, arrnew, arrerr)
 			{
 			status = cublasDcopy(handler, size, arrnew, 1, arrerr, 1);
 			if(status != CUBLAS_STATUS_SUCCESS){
+				printf("COPY ERROR!");
 				exit(EXIT_FAILURE);
 			}
 
 			status = cublasDaxpy(handler, size, &alpha, arrprev, 1, arrerr, 1);
 			if(status != CUBLAS_STATUS_SUCCESS){
+				printf("AXPY ERROR!");
 				exit(EXIT_FAILURE);
 			}
 
 			status = cublasIdamax(handler, size, arrerr, 1, &index);
 			if(status != CUBLAS_STATUS_SUCCESS){
+				printf("MAX ERROR!");
 				exit(EXIT_FAILURE);
 			}
 			}
+
+			for(int i = 0; i < size; i++){
+				for(int j = 0; j < size; j++){
+					#pragma acc kernels
+					printf("%lf ", arrerr[i * size + j]);
+				}
+				printf("\n");
+			}
+			printf("\n");
 
 			#pragma acc update host(arrerr[index - 1])
 			err = abs(arrerr[index - 1]);
 		}
 			
 
-		float** temp = arrprev;
+		//Array swap
+		float* temp = arrprev;
 		arrprev = arrnew;
 		arrnew = temp;
 
+		//Print the results every 1000 iterations
 		if(iter == 1 || iter % 1000 == 0){
 			printf("On %d iteration error equals %lf\n", iter, err);
 			clock_t mid = clock();
@@ -115,6 +157,8 @@ int main(int argc, char** argv){
 			printf("Time elapsed: %lf\n", te_mid);
 		}
 	}
+
+	//Results
 	printf("On %d iteration error descended to %lf\n", iter, err);
 	}
 
