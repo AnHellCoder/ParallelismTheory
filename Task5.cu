ded89552c872878e78aec84df7f7918239d3fadb
AnHellCoder <117171875+AnHellCoder@users.noreply.github.com>
2023-06-01 11:09:44 +0700

    Add files via upload

--- Task5.cu
+++ Task5.cu
@@ -26,7 +26,7 @@ int main(int argc,char *argv[]){
         clock_t begin = clock();
 
         //MPI initialization
-        int rank, group_size;
+        int rank, group_size, status;
         MPI_Request request;
         MPI_Init(&argc,&argv);
 
@@ -39,6 +39,7 @@ int main(int argc,char *argv[]){
         if (deviceCount<group_size) throw runtime_error("Too many MPI threads");
 
         cudaSetDevice(rank);
+        cout << "Your rank is " << rank << ", group_size is " << group_size << endl;
 
         //Set p2p access
         if (rank!=0) cudaDeviceEnablePeerAccess(rank-1,0);
@@ -78,7 +79,6 @@ int main(int argc,char *argv[]){
         if (rank==group_size-1) end-=1;
 
         int size_per_gpu = end-start;
-
         if (group_size==1) size_per_gpu=size;
 
         DEVICE_MALLOC(double,arrprev,(size * size))
@@ -91,15 +91,15 @@ int main(int argc,char *argv[]){
         memset(arr, 0, (size * size)*sizeof(double));
 
         //Threads and blocks initialization
-        int threads_x= size < 1024 ? size : 1024;
+        int threads_x = size < 1024 ? size : 1024;
         int blocks_y = size_per_gpu;
         int blocks_x = size/threads_x;
 
-        dim3 blocks(blocks_x,blocks_y);
+        dim3 blocks(blocks_x, blocks_y);
         dim3 threads(threads_x,1);
 
         //Temporary array initialization (fill)
-        double step = (double)10/size;
+        double step = (double)10/(size-1);
         int k = size - 1;
         arr[0] = 10;
         arr[size - 1] = 20;
@@ -138,40 +138,54 @@ int main(int argc,char *argv[]){
                         cudaMemcpy(&loss,cudaLoss, sizeof(double), cudaMemcpyDeviceToHost);
 
                         //Sending loss to all processes
-                        MPI_Allreduce(&loss,&loss_buff,1,MPI_DOUBLE,MPI_MAX,MPI_COMM_WORLD);
+                        status = MPI_Allreduce(&loss,&loss_buff,1,MPI_DOUBLE,MPI_MAX,MPI_COMM_WORLD);
+                        if(status != MPI_SUCCESS){
+                                cout << "REDUCE ERROR!" << endl;
+                                return 1;
+                        }
 
                         //Intermediate results
                         clock_t mid = clock();
                         cout << "On " << iter << " iteration loss equals: " << loss << endl;
                         cout << "Time elapsed: " << (double)(mid - begin)/CLOCKS_PER_SEC << endl;
+                        cout << "Computed on " << rank << " device" << endl;
                }
 
-                //Exchanging matrix rows between ranks
-                if (rank!=group_size-1){
-                        MPI_Isend(&arrnew[(size_per_gpu-2)*size+1],size-2,MPI_DOUBLE,rank+1,0,MPI_COMM_WORLD,&request);
-                        MPI_Recv(&arrnew[(size_per_gpu-1)*size+1],size-2,MPI_DOUBLE,rank+1,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-                }
-                if (rank!=0){
-                        MPI_Isend(&arrnew[size+1],size-2,MPI_DOUBLE,rank-1,0,MPI_COMM_WORLD,&request);
-                        MPI_Recv(&arrnew[1],size-2,MPI_DOUBLE,rank-1,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-                }
+               //Exchanging matrix rows between ranks
+               if (rank!=group_size-1){
+                       MPI_Isend(&arrnew[(size_per_gpu-2)*size+1],size-2,MPI_DOUBLE,rank+1,0,MPI_COMM_WORLD,&request);
+                       MPI_Recv(&arrnew[(size_per_gpu-1)*size+1],size-2,MPI_DOUBLE,rank+1,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+                       //status = MPI_Sendrecv(&arrnew[(size_per_gpu-2)*size+1],size-2,MPI_DOUBLE,rank+1,0,&arrnew[(size_per_gpu-1)*size+1],size-2,MPI_DOUBLE,rank+1,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+               }
+               if (rank!=0){
+                       MPI_Isend(&arrnew[size+1],size-2,MPI_DOUBLE,rank-1,0,MPI_COMM_WORLD,&request);
+                       MPI_Recv(&arrnew[1],size-2,MPI_DOUBLE,rank-1,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+                       //status = MPI_Sendrecv(&arrnew[size+1],size-2,MPI_DOUBLE,rank-1,0,&arrnew[1],size-2,MPI_DOUBLE,rank-1,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+               }
 
-                swap(arrprev,arrnew);
+               swap(arrprev,arrnew);
         }
 
         //Results
+        if(rank != 0) MPI_Send(&loss,1,MPI_DOUBLE,0,0,MPI_COMM_WORLD);
+        else{
+                double loss_import;
+                for(int i = 1; i < group_size; i++){
+                        MPI_Recv(&loss_import,1,MPI_DOUBLE,i,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+                        loss = max(loss, loss_import);
+                }
+                clock_t te = clock();
+                cout << "On " << iter << " iteration loss descended to " << loss << endl;
+                cout << "Time elapsed: " << (double)(te - begin)/CLOCKS_PER_SEC << endl;
+        }
+
         cudaFree(arrprev);
         cudaFree(arrnew);
         cudaFree(arrloss);
         cudaFree(cudaLoss);
         delete[] arr;
-        MPI_Finalize();
 
-        if(rank == 0){
-                clock_t te = clock();
-                cout << "On " << iter << " iteration loss descended to " << loss << endl;
-                cout << "Time elapsed: " << (double)(te - begin)/CLOCKS_PER_SEC << endl;
-        }
+        MPI_Finalize();
 
         return 0;
 }
